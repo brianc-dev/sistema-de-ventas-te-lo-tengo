3e8ee9e26c7e64c46e0fe5793c57131efc96ef77
Brian Caldera <brian_alex07@outlook.com>
2023-05-14 22:49:34 -0400

    Created models and migrations

--- database/migrations/2023_05_14_063227_create_clientes_table.php
+++ database/migrations/2023_05_14_063227_create_clientes_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clientes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->boolean('active');
+            $table->integer('cedula');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('email');
+            $table->string('telefono');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clientes');
     }
 };
